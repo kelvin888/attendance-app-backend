0225dca2ed5924e36eec22cd3596c4ebc480d3b0
Orhungul Kelvin <kelvin.orhungul@africaprudential.com>
2020-03-31 15:33:06 +0100

    initial spring boot project

---
+++ src/main/java/com/kelvin/attendance/AttendanceAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kelvin.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceAppBackendApplication.class, args);
+	}
+
+}
